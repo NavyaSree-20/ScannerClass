bcef7431872ef02fe1b7adcc080f3d13eddcff6d
NavyaSree <gnavyasree20@gmail.com>
2024-06-27 14:40:26 +0530

    Adding the values

---
+++ src/Name.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name1=sc.nextLine();
+        System.out.print("Enter your name:");
+        String name2=sc.nextLine();
+        System.out.println(name1+" and "+name2);
+    }
+}
