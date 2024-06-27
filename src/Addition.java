1a5d3ab163b9c2a88b141b882c8fb65c9873753e
NavyaSree <gnavyasree20@gmail.com>
2024-06-27 13:55:45 +0530

    Adding the values

---
+++ src/Addition.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number1:");
+        int num1=sc.nextInt();
+        System.out.print("Enter the number2:");
+        int num2=sc.nextInt();
+        System.out.println("Addition: "+(num1+num2));
+
+    }
+}
